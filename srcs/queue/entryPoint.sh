3de230bcbe78c50749775b5eb667cca2eed78658
isayen <ismailsvn02@gmail.com>
2026-06-14 23:00:16 +0100

    Reorganize project structure and customizes queue
    Relocate database configuration and initialization scripts into a dedicated source directory to improve project organization.
    
    Replace the generic RabbitMQ image with a custom build to automate plugin enablement and user permission configuration during the container startup process.

---
+++ srcs/queue/entryPoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+mkdir -p /etc/rabbitmq
+
+rabbitmq-plugins enable rabbitmq_management
+
+echo 'loopback_users.guest = false' >> /etc/rabbitmq/rabbitmq.conf
+
+sudo rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASS || true
+
+sudo rabbitmqctl set_permissions -p $RABBITMQ_VHOST $RABBITMQ_USER ".*" ".*" ".*" || true
+
+exec rabbitmq-server
